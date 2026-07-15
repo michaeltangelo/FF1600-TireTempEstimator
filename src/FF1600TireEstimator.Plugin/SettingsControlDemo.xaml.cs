3d277f285cc2045e8d83c31483c0c1e7b6796810
michaeltangelo <the.michaeltangelo@gmail.com>
2026-07-14 23:29:57 -0400

    Add verified SimHub SDK template baseline

---
+++ src/FF1600TireEstimator.Plugin/SettingsControlDemo.xaml.cs
@@ -0,0 +1,47 @@
+﻿using SimHub.Plugins.Styles;
+using System.Windows.Controls;
+using WoteverLocalization;
+
+namespace User.PluginSdkDemo
+{
+    /// <summary>
+    /// Logique d'interaction pour SettingsControlDemo.xaml
+    /// </summary>
+    public partial class SettingsControlDemo : UserControl
+    {
+        public DataPluginDemo Plugin { get; }
+
+        public SettingsControlDemo()
+        {
+            InitializeComponent();
+        }
+
+        public SettingsControlDemo(DataPluginDemo plugin) : this()
+        {
+            this.Plugin = plugin;
+        }
+
+        private async void StyledMessageBox_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var res = await SHMessageBox.Show("Message box", SLoc.GetValue("MyPlugin_LocalizedDialogTitle"), System.Windows.MessageBoxButton.OKCancel, System.Windows.MessageBoxImage.Question);
+
+            await SHMessageBox.Show(res.ToString());
+        }
+
+        private void DemoWindow_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var window = new DemoWindow();
+
+            window.Show();
+        }
+
+        private async void DemodialogWindow_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var dialogWindow = new DemoDialogWindow();
+
+            var res = await dialogWindow.ShowDialogWindowAsync(this);
+
+            await SHMessageBox.Show(res.ToString());
+        }
+    }
+}
