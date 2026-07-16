afbfa2efaf5154e4358d5140d5482b5a77319d4a
michaeltangelo <the.michaeltangelo@gmail.com>
2026-07-16 14:06:10 -0400

    Replace demo settings page with recorder controls

--- src/FF1600TireEstimator.Plugin/SettingsControlDemo.xaml.cs
+++ src/FF1600TireEstimator.Plugin/SettingsControlDemo.xaml.cs
@@ -1,15 +1,14 @@
-﻿using SimHub.Plugins.Styles;
+using System;
+using System.Globalization;
+using System.Windows;
 using System.Windows.Controls;
-using WoteverLocalization;
+using System.Windows.Threading;
 
 namespace FF1600TireEstimator.Plugin
 {
-    /// <summary>
-    /// Logique d'interaction pour SettingsControlDemo.xaml
-    /// </summary>
     public partial class SettingsControlDemo : UserControl
     {
-        public FF1600Tires Plugin { get; }
+        private readonly DispatcherTimer refreshTimer;
 
         public SettingsControlDemo()
         {
@@ -18,30 +17,69 @@ namespace FF1600TireEstimator.Plugin
 
         public SettingsControlDemo(FF1600Tires plugin) : this()
         {
-            this.Plugin = plugin;
+            Plugin = plugin;
+            EvidencePathValue.Text = plugin.EvidenceRoot;
+            refreshTimer = new DispatcherTimer(DispatcherPriority.Background)
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+            refreshTimer.Tick += RefreshTimer_Tick;
+            Loaded += SettingsControlDemo_Loaded;
+            Unloaded += SettingsControlDemo_Unloaded;
+            RefreshStatus();
         }
 
-        private async void StyledMessageBox_Click(object sender, System.Windows.RoutedEventArgs e)
-        {
-            var res = await SHMessageBox.Show("Message box", SLoc.GetValue("MyPlugin_LocalizedDialogTitle"), System.Windows.MessageBoxButton.OKCancel, System.Windows.MessageBoxImage.Question);
+        public FF1600Tires Plugin { get; }
 
-            await SHMessageBox.Show(res.ToString());
+        private void SettingsControlDemo_Loaded(object sender, RoutedEventArgs e)
+        {
+            refreshTimer?.Start();
+            RefreshStatus();
         }
 
-        private void DemoWindow_Click(object sender, System.Windows.RoutedEventArgs e)
+        private void SettingsControlDemo_Unloaded(object sender, RoutedEventArgs e)
         {
-            var window = new DemoWindow();
-
-            window.Show();
+            refreshTimer?.Stop();
         }
 
-        private async void DemodialogWindow_Click(object sender, System.Windows.RoutedEventArgs e)
+        private void RefreshTimer_Tick(object sender, EventArgs e)
         {
-            var dialogWindow = new DemoDialogWindow();
+            RefreshStatus();
+        }
 
-            var res = await dialogWindow.ShowDialogWindowAsync(this);
+        private void StartCapture_Click(object sender, RoutedEventArgs e)
+        {
+            Plugin.StartTelemetryCapture();
+            RefreshStatus();
+        }
 
-            await SHMessageBox.Show(res.ToString());
+        private void StopCapture_Click(object sender, RoutedEventArgs e)
+        {
+            Plugin.StopTelemetryCapture();
+            RefreshStatus();
+        }
+
+        private void ToggleCapture_Click(object sender, RoutedEventArgs e)
+        {
+            Plugin.ToggleTelemetryCapture();
+            RefreshStatus();
+        }
+
+        private void RefreshStatus()
+        {
+            if (Plugin == null)
+            {
+                return;
+            }
+
+            var status = Plugin.RecorderStatus;
+            StateValue.Text = status.State;
+            RequestedValue.Text = status.Requested.ToString();
+            RunIdValue.Text = status.RunId == Guid.Empty ? "<none>" : status.RunId.ToString("D");
+            AcceptedValue.Text = status.Accepted.ToString(CultureInfo.InvariantCulture);
+            RejectedValue.Text = status.Rejected.ToString(CultureInfo.InvariantCulture);
+            FaultValue.Text = string.IsNullOrEmpty(status.Fault) ? "<none>" : status.Fault;
+            DiagnosticsValue.Text = Plugin.GetDiscoverySummary();
         }
     }
 }
